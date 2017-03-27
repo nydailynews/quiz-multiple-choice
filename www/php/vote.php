2c180a47ab5cb729b334a2dd423b7edb7a1bd5de
freejoe76 <jmurphy@denverpost.com>
2017-03-27 11:16:16 -0400

    Make the file that handles the votes look respectable.

--- www/php/vote.php
+++ www/php/vote.php
@@ -1,30 +1,34 @@
 <?php
+// This file assumes it is being included from a quiz-specific vote handler,
+// which will pass it the variable for the table name and the env var
+// for the server_root.
+if ( !isset($SERVER_ROOT) ) $SERVER_ROOT = '';
+if ( !isset($TABLE) ) exit('');
+
 $vote = intval($_REQUEST['vote']);
 $player = htmlspecialchars($_REQUEST['player']);
 
-require_once ('../../../../includes/php/mysql_connect_production.php')
+require_once ($SERVER_ROOT . 'includes/php/mysql_connect_production.php')
 
-$query = "SELECT * FROM giants_kd_2016 WHERE name = ".$player;
+$query = 'SELECT * FROM $TABLE WHERE name = "' . $player . '" LIMIT 1';
 $result = @mysql_query($query);
 
-while ($row = mysql_fetch_array($result, MYSQL_ASSOC)){    
-  $yes = $row['keep'];
-  $no = $row['dump']; 
+while ($row = mysql_fetch_array($result, MYSQL_ASSOC)):
+	$yes = $row['keep'];
+	$no = $row['dump']; 
+endwhile;
 
+if ($vote == 0):
+	$value = $yes + 1;
+	$verb = 'keep';
+	echo $value."||".$no;
+elseif ($vote == 1):
+	$value = $no + 1;
+	$verb = 'dump';
+	echo $yes."||".$value;
+endif;
 
-}
-
-if ($vote == 0) {
-  $yes = $yes + 1;
-  $update = "UPDATE Interactive.giants_kd_2016 SET keep=".$yes." WHERE name = ".$player;
-  $result = @mysql_query($update);
-}
-if ($vote == 1) {
-  $no = $no + 1;
-  $update = "UPDATE Interactive.giants_kd_2016 SET dump=".$no." WHERE name = ".$player;
-  $result = @mysql_query($update);
-}
-
-echo $yes."||".$no;
+$update = 'UPDATE Interactive.' . $TABLE . ' SET ' . $verb . '=' . $value . ' WHERE name = "' . $player . '" LIMIT 1';
+$result = @mysql_query($update);
 
 ?>

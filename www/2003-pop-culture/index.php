5ec5df93ea6ae093e3711116e0beb7c6c656270c
Kelli Parker <iamkelliparker@gmail.com>
2018-01-26 11:32:45 -0500

    adding the 2003 pop culture quiz

---
+++ www/2003-pop-culture/index.php
@@ -0,0 +1,196 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>2003 Pop Culture, by the NY Daily News</title>
+    <link rel="icon" type="image/png" href="//interactive.nydailynews.com/favicons.png">
+    <!-- DEFAULT -->
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="apple-mobile-web-app-capable" content="yes" />
+    <meta name="apple-mobile-web-app-status-bar-style" content="black" />
+    <link rel="apple-touch-icon" href="">
+    <meta name="format-detection" content="telephone=no"/>
+    <meta http-equiv="X-UA-Compatible" content="IE=Edge" />
+
+    <!-- Titles -->
+    <meta property="og:title" content='2003 Pop Culture' />
+    <meta name="twitter:title" content='2003 Pop Culture'/>
+
+    <!-- Description -->
+    <meta name="description" content="It has been 15 years since New York City hosted the Grammy Awards. How well do you remember 2003?" />
+    <meta property="og:description" content="It has been 15 years since New York City hosted the Grammy Awards. How well do you remember 2003?" />
+    <meta name="twitter:description" content="It has been 15 years since New York City hosted the Grammy Awards. How well do you remember 2003?" />
+
+    <!-- KEYWORD -->
+    <meta name="keywords" content="2003, pop culture, 2018 grammy awards, the grammys, quiz" />
+    <meta name="news_keywords" content="2003, pop culture, 2018 grammy awards, the grammys, quiz" />
+
+    <!-- LINK -->
+    <link rel="canonical" href="http://interactive.nydailynews.com/quiz/2003-pop-culture/">
+    <meta property="og:url" content="http://interactive.nydailynews.com/quiz/2003-pop-culture/" />
+    <meta name="twitter:url" content="http://interactive.nydailynews.com/quiz/2003-pop-culture/">
+
+    <!-- THUMBNAIL IMAGE-->
+    <meta property="og:image" content="http://interactive.nydailynews.com/quiz/2003-pop-culture/img/splitter.jpg" />
+    <meta name="twitter:image:src" content="http://interactive.nydailynews.com/quiz/2003-pop-culture/img/splitter.jpg" />
+    <meta property="og:image:width" content="1024" />
+    <meta property="og:image:height" content="512" />
+
+    <!-- PARSELY -->
+    <script type="application/ld+json">
+        {
+            "@context": "http://schema.org",
+            "@type": "NewsArticle",
+            "headline": "2003 Pop Culture",
+            "url": "http://interactive.nydailynews.com/quiz/2003-pop-culture/",
+            "thumbnailUrl": "http://interactive.nydailynews.com/quiz/2003-pop-culture/img/splitter.jpg",
+            "dateCreated": "DATET07:00:00Z",
+            "articleSection": "Interactive",
+            "creator": ["Virginia Lowman","Kelli R. Parker","Interactive Quiz"],
+            "keywords": ["interactive","interactive quiz","2018 grammys","2003 pop culture"]
+        }
+    </script>
+
+    <!-- NO NEED TO FILL -->
+    <meta name="twitter:card" content="summary_large_image">
+    <meta name="twitter:domain" content="http://interactive.nydailynews.com"/>
+    <meta name="twitter:site" content="@nydailynews">
+    <meta name="decorator" content="responsive" />
+    <meta name="nydn_section" content="NY Daily News" />
+    <meta name="robots" content="NOARCHIVE" />
+    <meta name="msvalidate.01" content="02916AAC0DA8B068EFE01D721E03ED7E" />
+    <meta name="p:domain_verify" content="78efe4f5c9935744af497772f68a0ee7" />
+    <meta property="fb:app_id" content="107464888913" />
+    <meta property="fb:admins" content="1594068001" />
+    <meta property="fb:pages" content="268914272540" />
+    <meta property="og:type" content="article" />
+    <meta property="og:site_name" content="NY Daily News" />
+    <meta property="article:publisher" content="https://www.facebook.com/NYDailyNews/" />
+    <meta name="themeKey" content="nydailynews" />
+    <meta name="mapThemeKey" content="responsive" />
+    <meta name="urlPrefix" content="" />
+    <meta name="localeCountry" content="US"/>
+    <meta name="localeLanguage" content="en" />
+
+<?php if ( !isset($_GET['embed']) ): ?>
+    <!-- ADOBE ANALYTICS -->
+    <script src="//assets.adobedtm.com/4fc527d6fda921c80e462d11a29deae2e4cf7514/satelliteLib-c91fdc6ac624c6cbcd50250f79786de339793801.js"></script>
+<?php endif; ?>
+
+    <link href='https://fonts.googleapis.com/css?family=Open+Sans:400,800' rel='stylesheet'>
+    <script data-main="//www.nydailynews.com/nydn/js/rh.js?r=20170405001" src="//www.nydailynews.com/nydn/js/require.js?r=2016LIST" defer></script>
+      
+    <script src="config.js"></script>
+    <link rel="stylesheet" href="//www.nydailynews.com/nydn/c/rh.css">
+    <link rel="stylesheet" href="/css/foundation.css">
+    <link href="../css/style.css?v201707" rel="stylesheet">
+    <script>
+    var is_iframe = 0;
+    if ( window.top !== window.self ) { is_iframe = 1; }
+    var nydn = nydn || {
+        "section": "NYDailyNews",
+        "subsection": "news",
+        "template": "article",
+        "revision": "201609014009",
+        "bidder": { contains: function() {} },
+        "targetPath": document.location.pathname
+    };
+    </script>
+    <script>
+    var nydnDO = [
+        { 
+            'title':'xxx2003 Pop Culturexxx', 
+            'link':'http://interactive.nydailynews.com/quiz/2003-pop-culture/', 
+            'p_type':'interactive', 
+            'section':'interactive' }
+    ];
+    </script>
+    
+    <!-- ADS-START -->
+        <script>
+        var ad_tag = 'quiz';
+        if ( typeof appConfig.ad_tag !== 'undefined' ) ad_tag += '-' + appConfig.ad_tag;
+        </script>
+        <script onload="nydn_ads(ad_tag);" src="/includes/ads/ads.js"></script>
+    <!-- ADS-END -->
+    <script>var nav_params = {section: 'quizzes', url: 'http://interactive.nydailynews.com/quiz/'};</script>
+    <script src="/library/vendor-nav/vendor-include.js" defer></script>
+</head>
+<body id="nydailynews" data-section="nydailynews" data-subsection="NY Daily News" class="">
+
+<!-- SITEHEADER-START -->
+<header id="templateheader"></header>
+<!-- SITEHEADER-END -->
+
+
+<main>
+  <article>
+    <h1 class="center">QUIZ: 2003 Pop Culture</h1>
+    <p class="center description">It has been 15 years since New York City last hosted the Grammy Awards, and a lot has certainly changed since then. Here’s a quick flashback to test your knowledge of what music, pop culture, and the world was like in 2003. What do you remember?</p>
+    <p class="byline center">By VIRGINIA LOWMAN</p>
+    <p class="center" id="result"></p>
+
+    <section id="content">
+        <div class="row">   
+            <div class="headline large-12 medium-12 small-12 columns center"></div>
+            <div class="description large-12 medium-12 small-12 columns center"></div>
+
+            <div class="large-12 medium-12 small-12 columns">
+                <div class="quiz-container"></div>
+            </div>
+
+            <div class='center next large-12 medium-12 small-12 columns'>
+                <div class="side_ad center" style="margin: 20px auto;">
+					<div id='div-gpt-ad-1423507761396-2'>
+                        <script>
+                            googletag.cmd.push(function() { googletag.display('div-gpt-ad-1423507761396-2'); });
+                        </script>
+                    </div>
+                    <button class='continue_btn'>CONTINUE QUIZ</button>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- CUSTOM JS GOES HERE-->
+    <script src="/js/jquery.min.js"></script>
+    <script src="../js/script.js?v5"></script>
+
+  </article>
+</main>
+
+<!--
+<div class="ad center">
+    <span>ADVERTISEMENT</span>
+    <div id='div-gpt-ad-1423507761396-1'>
+        <script>
+            googletag.cmd.push(function() { googletag.display('div-gpt-ad-1423507761396-1'); });
+        </script>
+    </div>
+</div>
+    
+<div class="ad center">
+    <div id='div-gpt-ad-x105'>
+        <script>
+            googletag.cmd.push(function() { googletag.display('div-gpt-ad-x105'); });
+        </script>
+    </div>
+</div>
+-->
+
+<!-- FOOTER-START -->
+<footer id="templatefooter"></footer>
+<!-- FOOTER-END -->
+<div id="ra-bp">
+      </div> <section id="rao">  <div id="rao-close"></div> <div id="rao-wrap"></div> </section> 
+<div id="r-scripts">
+    <div id="parsely-root" style="display: none">
+        <span id="parsely-cfg" data-parsely-site="nydailynews.com"></span>
+    </div>
+    <div class="r-ad">
+        <div id="div-gpt-ad-x100">
+        </div>
+    </div>
+</div>
+</body>
+</html>
